6c4a5eb5f6b38db55e57b6cda96f650cdd42fb4f
anandramz <aramaswamy23@gmail.com>
2026-09-08 19:20:14 -0400

    Time: 75 ms (88.29%) | Memory: 19.6 MB (52.11%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,12 +1,5 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # Pen & Paper 
-            # "AABAKABBA", k = 2
-            # current streak
-            # so we maintain what are the current characters in our hashmap and if we can replace them or not for our max character if we cannot then we contrain the hashmap untill we can 
-        # Bruteforce 
-        # Pattern 
-        # Implementation Plan 
         l = 0 
         map = defaultdict(int)
         max_value = 0
